e69ee513720e0a86e6f232f3611098e49ec59955
cospan <cospan@gmail.com>
2016-06-19 12:13:34 -0400

    Initial commit

---
+++ LogicAnalyzer/device/device.py
@@ -0,0 +1,22 @@
+
+
+
+class Device(object):
+
+    def __init__(self):
+        pass
+
+    def open(self):
+        pass
+
+    def get_capabilities(self):
+        pass
+
+    def set_trigger(self, trigger):
+        pass
+
+    def set_trigger_mask(self, trigger_mask):
+        pass
+
+    
+
